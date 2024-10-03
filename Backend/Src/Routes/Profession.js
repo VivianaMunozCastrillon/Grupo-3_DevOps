a9de12bcbb36281d4f9aba23a3a6588c5be51c4b
viviana <126583669+VivianaMunozCastrillon@users.noreply.github.com>
2024-10-03 11:21:06 -0500

    Revert "Feature/creation of rouetes and controllers (#16)"
    This reverts commit a06f9fac067ac87605164f1a82068f27fd540c94.

--- Backend/Src/Routes/Profession.js
+++
@@ -1,7 +0,0 @@
-/*const express = require('express');
-const router = express.Router();//Permite crear los endpoints con sus respectivos metodos
-const Professions = require('../Controllers/ControllerProfession')
-
-router.get('/AllProfessions', Professions );
-
-module.exports = router;*/
--- Backend/Src/Routes/WhoWeAre.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Home = require('../Controllers/ControllersHome')
-
-router.get('/Home', Home);
-
-module.exports = router;
