b807a0278b359a7c2c8609abbd8b6f56a499fb2d
Jaider-Oquendo <126584216+Jaider-Oquendo@users.noreply.github.com>
2024-10-03 12:43:10 -0500

    Feature/controllers and routes (#19)
     Creation of controllers and routes
    
    Co-authored-by: Viviana Muñoz <vivi84castri@gmail.com>

---
+++ Backend/Src/Routes/Candidate.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../middleware/multer');
+const { GetCandidates, DeleteCandidates } = require('../Controllers/ControllerAdministrator');
+const GetProfessions = require('../Controllers/ControllerProfession'); // Asegúrate de que este nombre coincida
+const RegisterCandidate = require('../Controllers/ControllerCandidate');
+const GetCitiesAndSkillsByProfession = require('../Controllers/ControllerCitySkill');
+
+// Ruta para obtener todas las profesiones
+router.get('/AllProfessions', GetProfessions); 
+//Obtener Ciudades y Habilidades Por Profesion
+router.get('/profession/:ProfessionId', GetCitiesAndSkillsByProfession); 
+
+//Registrar Candidatos
+router.post('/candidates', upload.single('resume'), RegisterCandidate);
+
+//Administrar Candidatos
+router.get('/AllCandidates', GetCandidates);
+router.delete('/DeleteCandidates/:CandidatesId', DeleteCandidates);
+
+module.exports = router;
+
